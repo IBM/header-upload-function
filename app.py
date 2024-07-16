16fd6c0a2268edaa7ce2ed2f45ca16d6b8066d7b
Gidwani Honey <Honey.Gidwani@ibm.com>
2024-07-16 16:35:23 +0530

    Directly parsed the file and updated response

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ import os
 import eml_parser
 import json
 import datetime
-# importing necessary functions from dotenv library
-from dotenv import load_dotenv, dotenv_values 
 app = Flask(__name__)
-
-load_dotenv()
 
 def json_serial(obj):
     if isinstance(obj, datetime.datetime):
@@ -38,19 +34,20 @@ def upload_file():
         #     endpoint_url=credentials['ENDPOINT']
         # )
         # cos_client.upload_fileobj(file, credentials['BUCKET'], file.filename)
-        print('file', file)
+        # print('file', file)
         raw_email = file.read()
-        print('raw_emailk', raw_email)
+        # print('raw_emailk', raw_email)
         ep = eml_parser.EmlParser()
         parsed_eml = ep.decode_email_bytes(raw_email)
-        print(f"File 'object_name uploaded successfully to bucket.")
-        parsed_json = json.dumps(parsed_eml, default=json_serial)      
+        
+        parsed_json = json.dumps(parsed_eml, default=json_serial)
+        print(f"File 'object_name uploaded successfully to bucket.", parsed_eml) 
         return {
             "headers": {
                 "Content-Type": "application/json",
             },
             "statusCode": 200,
-            "body": parsed_json,
+            "body": parsed_eml,
         }
     except Exception as e:
         print(f"Failed to upload file 'object_name to bucket {str(e)}")
